d5910374b2cab2b03e7010ed1e43bb21bf7fe706
Sowmya-257 <sowmyaaddala25@gmail.com>
2025-11-01 11:58:54 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (32.61%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,17 +1,50 @@
-class Solution {
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         if (strs.empty()) return ""; // Handle edge case of an empty array
+        
+//         string prefix = strs[0]; // Initialize prefix as the first string
+        
+//         for (int i = 1; i < strs.size(); i++) {
+//             // Reduce the prefix until it matches the start of strs[i]
+//             while (strs[i].find(prefix) != 0) {
+//                 prefix = prefix.substr(0, prefix.size() - 1); // Remove the last character
+//                 if (prefix.empty()) return ""; // If no common prefix exists
+//             }
+//         }
+//         return prefix;
+//     }
+// };
+
+
+class Solution{
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return ""; // Handle edge case of an empty array
-        
-        string prefix = strs[0]; // Initialize prefix as the first string
-        
-        for (int i = 1; i < strs.size(); i++) {
-            // Reduce the prefix until it matches the start of strs[i]
-            while (strs[i].find(prefix) != 0) {
-                prefix = prefix.substr(0, prefix.size() - 1); // Remove the last character
-                if (prefix.empty()) return ""; // If no common prefix exists
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];
+
+        for(int i=1; i< strs.size() ; i++){
+
+            while(strs[i].find(prefix) !=0){
+                prefix = prefix.substr(0, prefix.size()-1);
+                if(prefix=="") return "";
             }
         }
         return prefix;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
