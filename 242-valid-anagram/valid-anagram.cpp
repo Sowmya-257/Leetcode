4b80148786f17a2a85df68801986434b7ef932d1
Sowmya-257 <sowmyaaddala25@gmail.com>
2026-02-01 15:14:04 +0530

    Time: 4 ms (35.41%) | Memory: 9.5 MB (91.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        int n1= s.size();
+        int n2= t.size();
 
-        int count[26] = {0};
+        if(n1 != n2) return false;
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
+        for(int i=0 ; i<n1 ;i++){
+            if(s[i] != t[i]) return false;
         }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
-        }
-
-        return true;
+       return true;
     }
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  if (s.length() != t.length()) return false;
+
+//         int count[26] = {0};
+
+//         for (int i = 0; i < s.length(); i++) {
+//             count[s[i] - 'a']++;
+//             count[t[i] - 'a']--;
+//         }
+
+//         for (int i = 0; i < 26; i++) {
+//             if (count[i] != 0) return false;
+//         }
+
+//         return true;
