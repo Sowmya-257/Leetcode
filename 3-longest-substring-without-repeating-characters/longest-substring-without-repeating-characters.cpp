c96b2bc13fc6749ba1f92c3efb6e07289f5753bd
Sowmya-257 <sowmyaaddala25@gmail.com>
2026-02-01 12:21:21 +0530

    Time: 10 ms (50.00%) | Memory: 11.9 MB (44.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> seen;
-        int start =0;
-        int len=0;
-        for(int i=0;i<s.length();i++ ){
-            char curr= s[i];
-            if(seen.count(curr) && seen[curr] >= start){
-                start = seen[curr]+1;
-            }
-            seen[curr] = i;
-            len = max(len, i - start + 1);
-        }  
-        return len;
+       int n=s.size();
+       unordered_map<char,int> m;
+       int left=0;
+       int maxC=0;
+
+       for(int i=0;i<n;i++){
+           if( m.count(s[i]) && m[s[i]] >= left){
+                left = m[s[i]]+ 1;
+           }
+
+           m[s[i]]= i;
+           maxC= max(maxC, i-left+1);
+       }
+
+       return maxC;
     }
 };
 
@@ -20,6 +23,38 @@ public:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  unordered_map<char,int> seen;
+//         int start =0;
+//         int len=0;
+//         for(int i=0;i<s.length();i++ ){
+//             char curr= s[i];
+//             if(seen.count(curr) && seen[curr] >= start){
+//                 start = seen[curr]+1;
+//             }
+//             seen[curr] = i;
+//             len = max(len, i - start + 1);
+//         }  
+//         return len;
 
 
 
