ed4dbdfb11cbffe238fa54db4df69e1621318f57
Sowmya-257 <sowmyaaddala25@gmail.com>
2026-02-10 22:51:08 +0530

    Time: 3 ms (98.09%) | Memory: 11.3 MB (54.36%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -13,14 +13,14 @@ public:
         ListNode * fast= head;
 
         while(fast && fast->next){
-            slow= slow ->next;
-            fast = fast ->next ->next;
+            slow= slow -> next;
+            fast = fast -> next -> next;
 
             if(slow==fast){
                 ListNode* entry = head;
                 while (entry != slow) {
-                    entry = entry->next;
-                    slow = slow->next;
+                    entry = entry -> next;
+                    slow = slow -> next;
                 }
                 return entry ;
             };
